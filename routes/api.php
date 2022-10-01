61a19b3b8ff9377cb1cd4cc953e493d59481ec1a
Jose Emilio <j.e.ydarkserver@gmail.com>
2022-09-30 20:47:43 -0400

    Arreglado un bug con la ruta

--- routes/api.php
+++ routes/api.php
@@ -22,12 +22,14 @@ Route::resource('ruta', App\Http\Controllers\RutaController::class)->only(['inde
 Route::post('login', 'App\Http\Controllers\LoginController@login');
 Route::post('register', 'App\Http\Controllers\RegisterController@register');
 */
+Route::resource('ruta', App\Http\Controllers\RutaController::class)->only(['index', 'store', 'update', 'show', 'destroy']);
 Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
 Route::post('/login', [App\Http\Controllers\AuthController::class, 'login'])->name('login');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout']);
 });
